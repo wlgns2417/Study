2dfd805a4fc7a81799fe5208c658f35fb59a8e52
wlgns2417 <wlgns2417@gmail.com>
2026-09-14 15:06:23 +0900

    Use final conversational ranking filters

--- collect_words.py
+++ collect_words.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Entry point for the corpus-based conversational vocabulary builder."""
-from collect_words_v2 import main
+from collect_words_v3 import main
 
 if __name__ == "__main__":
     main()
